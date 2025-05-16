960dacece335c62ba1cb7fad608eb97b3d8fae5b
Sena <dimatesebas73@gmail.com>
2025-05-16 09:34:55 -0500

    Add Eventos and Administrador

--- src/main/java/com/BackNight/backendNIght/ws/repository/EventosRepository.java
+++ src/main/java/com/BackNight/backendNIght/ws/repository/EventosRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.BackNight.backendNIght.ws.entity.Eventos;
 
 public interface EventosRepository extends JpaRepository<Eventos, String> {
-
 }
